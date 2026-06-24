223a9e7b93637ab8bc6dafe4bb6d02a02769f508
Miguel <stormyth2305@gmail.com>
2026-06-24 11:14:13 +0200

    Ejercicio 12 completado

---
+++ ejercicio12.js
@@ -0,0 +1,31 @@
+const mutants = [
+  { name: 'Wolverine', power: 'regeneration' },
+  { name: 'Magneto', power: 'magnetism' },
+  { name: 'Professor X', power: 'telepathy' },
+  { name: 'Jean Grey', power: 'telekinesis' },
+  { name: 'Rogue', power: 'power absorption' },
+  { name: 'Storm', power: 'weather manipulation' },
+  { name: 'Mystique', power: 'shape-shifting' },
+  { name: 'Beast', power: 'superhuman strength' },
+  { name: 'Colossus', power: 'steel skin' },
+  { name: 'Nightcrawler', power: 'teleportation' }
+];
+
+function findMutantByPower(mutants, power) {
+  poder = [];
+  for (let i = 0; i < mutants.length; i++){
+    if(mutants[i].power === power){
+      poder.push(mutants[i].name);
+    } 
+  }
+
+
+  if(poder.length > 0){
+    console.log("Mutantes encontrados", poder);
+  } else {
+    console.log("No se ha encontro ningun mutante con ese poder")
+  }
+  return poder;
+}
+
+findMutantByPower(mutants, "steel skin");
